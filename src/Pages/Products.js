7ef26ca9741d8a0fbc9cafd7bcc3f8390184069e
joyan11 <joyan.s03@gmail.com>
2021-08-08 12:38:18 +0530

    changes

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,9 +1,10 @@
 /** @format */
 import data from "../data/data.json";
 import { ProductCard } from "../Components/Product/ProductCard";
+import React from "react";
 export const Products = () => {
-  const renderedProducts = data.productdata.map((item) => (
-    <ProductCard item={item} />
-  ));
+  const renderedProducts = React.Children.toArray(
+    data.productdata.map((item) => <ProductCard item={item} />)
+  );
   return <div className="product-listing">{renderedProducts}</div>;
 };
